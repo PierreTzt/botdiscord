cbdcccf5fcac4010b24c1c2e4768484e7bd2eeb9
cornergeek <76401152+cornergeek@users.noreply.github.com>
2020-12-21 14:50:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-const { Client } = require('discord.js');
+const { Client, Message } = require('discord.js');
 const client = new Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('message', message => {
+    if (message.content === 'ping') message.reply ('Jsuis pas ton pote')
   }
-});
+);
 
 client.login(process.env.TOKEN);
