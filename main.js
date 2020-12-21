ea8b45a83991af908724a5e4c03b3821d4c5db9d
cornergeek <76401152+cornergeek@users.noreply.github.com>
2020-12-21 14:30:13 +0100

    Reprise de l'exemple discord.js

--- main.js
+++ main.js
@@ -1,44 +1,14 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
 
-
-
-require("../modules/dashbord.js")(this.client);
-
-// const { prefix, token } = require('./config.json');
-
-
-
-const bot = new Discord.Client();
-
-bot.on('ready', () => {
-    console.log('Ready');
-    bot.user.setPresence({ activity: { name: 'regarde les étoiles'}}).catch(console.error);
-})
-
-bot.on('message', (message) => {
-
-    if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
-
-    const args = message.content.slice(process.env.PREFIX.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if (command === `ping`) {
-        message.reply('Jsuis pas ton pote !');
-    }
-    else if (command === `server`) {
-        message.channel.send(`nom du serveur : ${message.guild.name}\n Nombre d'utilisateurs : ${message.guild.memberCount}`);
-    }
-    else if (command === `avatar`) {
-        if (!message.mentions.users.size) {
-            return message.channel.send(`Votre avatar est : ${message.author.displayAvatarURL({format: 'png'})}`)
-        }
-
-        const avatarList = message.mentions.users.map(user => {
-            return `L'avatar de ${user.username} est : ${user.displayAvatarURL({format: 'png'})}`;
-        });
-
-        message.channel.send(avatarList);
-    }
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-bot.login(process.env.TOKEN);
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('process.env.TOKEN');
