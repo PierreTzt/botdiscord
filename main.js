92f43246158d4ca6d0bc2d581f61ed6192474d39
cornergeek <76401152+cornergeek@users.noreply.github.com>
2020-12-21 15:38:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,15 +11,19 @@ client.on('message', message => {
 
     const args = message.content.slice(PREFIX.length).split(/ +/);
 
-    console.log(args);
+//    console.log(args);
 
     const command = args.shift().toLowerCase();
 
-    console.log(command);
+//    console.log(command);
 
     if (command === 'Hey') message.channel.send ("Hey !");
     if (command === 'Bonjour') message.channel.send (`Bonjour de la part de tout ${message.guild.name}.`);
     if (command === 'Salut') message.channel.send (`Salut ${message.author.tag}`);
+    if (command === 'userinfo') {
+        const user_mention = message.mentions.users.first();
+        message.channel.send(`Voici le tag de la personne mentionnée : ${user_mention.tag}.`)
+    }
 });
 
 client.login(process.env.TOKEN);
