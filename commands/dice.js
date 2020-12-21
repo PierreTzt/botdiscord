5f5e73093a20a1c2a21f270fd5c23772b5acd5fe
cornergeek <76401152+cornergeek@users.noreply.github.com>
2020-12-21 16:46:53 +0100

    Create dice.js

---
+++ commands/dice.js
@@ -0,0 +1,26 @@
+const { MessageEmbed, MessageAttachment } = require("discord.js");
+const diceImg = new MessageAttachment('./assets/img/logo.jpg');
+const randomdice = () => Math.floor(Math.random() * 6) + 1;
+
+module.exports = {
+  name : `dice`,
+  desciption: `Renvoi un chiffre au hasard`,
+  execute(client, message, args) {
+      const embed = new MessageEmbed()
+        .setColor("#dc198c")
+        .setTitle("Random dice")
+        .attachFiles(diceImg)
+        .setThumbnail('attachment://logo.jpg')
+        .addFields(
+          { name : 'D1', value : randomDice(), inline: true },
+          { name : 'D2', value : randomDice(), inline: true },
+          { name : 'D3', value : randomDice(), inline: true }
+        )
+        .addFields(
+          { name : 'D4', value : randomDice(), inline: true },
+          { name : 'D5', value : randomDice(), inline: true },
+          { name : 'D6', value : randomDice(), inline: true }
+        )
+        message.channel.send(embed);
+  }
+}
