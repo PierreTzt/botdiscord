90b239eb42318a56d528ad8f801b4702732288c6
cornergeek <76401152+cornergeek@users.noreply.github.com>
2020-12-22 20:08:36 +0100

    Ajout de rôle

---
+++ commands/add.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name : `add`,
+  desciption: `Ajouter un rôle`,
+  execute(client, message, args) {
+        let role = message.guild.roles.cache.find(r => r.name === args.toString());
+        if (role) {
+          if (message.member.roles.cache.has(role.id)) return message.channel.send("Vous avez déjà ce rôle !");
+          if (role.permissions.has('KICK_MEMBERS')) return message.channel.send("Vous ne pouvez pas avoir ce rôle");
+
+          message.member.role.add(role)
+            .then(n => message.channel.send(`Vous possédez maintenant le rôle ${role}.`))
+            .cath(e => console.log(e));
+        } else {
+          message.channel.send("Le rôle n'existe pas!");
+        }
+  }
+}
