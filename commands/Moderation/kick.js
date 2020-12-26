d1c70d4ac3bd1857fa756efa0a32a932c38f2792
cornergeek <76401152+cornergeek@users.noreply.github.com>
2020-12-26 16:42:31 +0100

    Channel de log

--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -1,7 +1,19 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports.run = (client, message, args) => {
   const user = message.mentions.users.first ();
-  const reason = args.splice(1).join(' ');
+  const reason = (args.splice(1).join(' ') || 'Aucune raison spécifiée');
   user ? message.guild.member(user).kick(reason) : message.channel.send("L'utilisateur n'existe pas");
+
+  const embed = new MessageEmbed()
+    .setAuthor(`${user.username} (${user.id})`)
+    .setColor("#ffa500")
+    .setDescription(`**Action** : kick\n**Raison** : ${reason}`)
+    .setThumbnail(user.avatarURL())
+    .setTimestamp()
+    .setFooter(message.author.username, message.author.avatarURL());
+
+  client.channels.cache.get('779443908647714819').send(embed);
 };
 
 module.exports.help = {
