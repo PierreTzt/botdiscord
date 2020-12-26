e130b21a778db144d0d6cb43ec75692a22bec4f4
cornergeek <76401152+cornergeek@users.noreply.github.com>
2020-12-26 16:43:26 +0100

    Update ban.js

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports.run = (client, message, args) => {
   const user = message.mentions.users.first ();
-  const reason = args.splice(1).join(' ');
+  const reason = (args.splice(1).join(' ') || 'Aucune raison spécifiée');
   user ? message.guild.member(user).ban(reason) : message.channel.send("L'utilisateur n'existe pas");
 
   const embed = new MessageEmbed()
