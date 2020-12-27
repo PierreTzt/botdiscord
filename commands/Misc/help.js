1e3bd90f778dff07634cbbbd369c635e8b7aa04f
cornergeek <76401152+cornergeek@users.noreply.github.com>
2020-12-27 11:17:25 +0100

    Update help.js

--- commands/Misc/help.js
+++ commands/Misc/help.js
@@ -4,10 +4,12 @@ const { readdirSync } = require("fs");
 const categoryList = readdirSync('./commands');
 
 module.exports.run = (client, message, args) => {
+console.log(client.commands.filter(cat => cat.help.category === 'misc'));
+
   if (!args.length) {
     const embed = new MessageEmbed()
-    .setColor("#36393F")
-    .addField("Liste des commandes", `Une liste de toutes les sous-catégories disponibles et leurs commandes\nPour plus d'informations sur une commande, taper \`${PREFIX}help <command_name>\``)
+      .setColor("#36393F")
+      .addField("Liste des commandes", `Une liste de toutes les sous-catégories disponibles et leurs commandes\nPour plus d'informations sur une commande, taper \`${PREFIX}help <command_name>\``)
 
     for (const category of categoryList) {
       embed.addField(
