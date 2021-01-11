43bf117d9d8830f1b824fba8bef9d0f26c5efdb7
cornergeek <76401152+cornergeek@users.noreply.github.com>
2021-01-11 17:08:17 +0100

    Create channelCreate.js

---
+++ events/logs/channelCreate.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = async (client, channel) => {
+  const fetchGuildAuditLogs = await channel.guild.fetchGuildAuditLogs({
+    limit: 1,
+    type : 'CHANNEL_CREATE'
+  });
+
+  const latestChannelCreated = fetchGuildAuditLogs.entries.first();
+  console.log(latestChannelCreated);
+  const { executor } = latestChannelCreated;
+
+  const embed = new MessageEmbed()
+  .setAuthor("Création d'un nouveau salon")
+  .setColor("#dc143c")
+  .setDescription(`**Action** : Création de salon\n**Salon créé** : ${channel.name}`)
+  .setTimestamp()
+  .setFooter(executor.username, executor.displayAvatarURL());
+
+client.channels.cache.get('779443908647714819').send(embed);
+}
