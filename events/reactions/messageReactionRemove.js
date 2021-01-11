bebf005546dab424e3ce403108bc6e41e77ee852
cornergeek <76401152+cornergeek@users.noreply.github.com>
2021-01-11 19:20:34 +0100

    Update messageReactionRemove.js

--- events/reactions/messageReactionRemove.js
+++ events/reactions/messageReactionRemove.js
@@ -1,5 +1,19 @@
 const { MessageReaction } = require("discord.js")
 
 module.exports = (client, MessageReaction, user) => {
+  const message = MessageReaction.message;
+  const member = message.guild.members.cache.get(user.id);
+  const emoji = MessageReaction.emoji.name;
+  const channel = message.guild.channels.cache.find(c => c.id === '779443908647714819')
+  const fortniteRole = message.guild.roles.cache.get("778974539048419359");
+  if(member.user.bot) return;
 
+  if(["Candycane"].includes(emoji) && message.channel.id === channel.id) {
+    switch (emoji) {
+      case "Candycane" :
+        member.roles.remove(fortniteRole);
+        message.channel.send(`Le rôle ${fortniteRole.name} a été supprimé avec succès!`);
+        break;
+    }
+  }
 }
