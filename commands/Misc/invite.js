bbd99238abb6a31c3111f1019a6f5b70815a6609
cornergeek <76401152+cornergeek@users.noreply.github.com>
2021-01-13 15:59:56 +0100

    Update invite.js

--- commands/Misc/invite.js
+++ commands/Misc/invite.js
@@ -4,20 +4,22 @@ module.exports.run = (client, message, args) => {
     
 const embed = new MessageEmbed()
     .setColor(`BLUE`) // ou .setColor(`#0099ff`)
-    .setTitle(`Titre du message, maximum 256 caractères`)
-    // .setAuthor(`Nom de l'auteur`, `https://mtxserv.com/build/img/favicon/favicon.ico`, `https://mtxserv.com/fr/`)
+    .setTitle(`INVITATION`)
+    .setAuthor(`Corner Geek`, `https://cdn.discordapp.com/emojis/790939549681057814.png`)
     // .setAuthor(`${this.client.user.tag}`, `${this.client.user.displayAvatarURL()}`, 'https://mtxserv.com/fr/')
-    .setDescription(`Message contenu dans l'embed, maximum 2048 caractères`)
-    .setFooter("Pied de page du message")
+    .setDescription(`👍 Tu souhaites nous soutenir ?\n👍 Envie de nous donner un coup de pouce ?\n`)
+    // .setFooter("Pied de page du message")
     .setImage(`https://mtxserv.com/uploads/cover/creer-un-bot-discord-avec-discord-js-discord-191c77d00c4d79bf822422d6a05496bd.jpg`)
-    .setThumbnail(`https://mtxserv.com/uploads/banners/ae49ad104085151cbb44e27fffd9f16862cb6f2c.png`)
-    .setTimestamp() // Vous pouvez passer un objet Date() en argument
+    // .setThumbnail(`https://mtxserv.com/uploads/banners/ae49ad104085151cbb44e27fffd9f16862cb6f2c.png`)
+    // .setTimestamp() // Vous pouvez passer un objet Date() en argument
     // Fields
     // Sur une ligne complète :
-    .addField(`Titre, maximum 256 caractères`,`Votre texte, maximum 1024 caractères`)
+    .addField(`Tu peux le faire en votant pour nous ! N'hésite pas !`,`Cela prends **2 secondes** et nous apporte une plus grande visibilité pour pouvoir amener plus de monde ! `)
     // Plusieurs sur une même ligne :
-    .addField(`Titre 1`,`Votre texte 1`, true)
-    .addField(`Titre 2`,`Text avec un [lien](https://mtxserv.com/fr/)`, true)
+    .addField(`Top-Serveur`,`[ici](https://top-serveurs.net/discord/corner-geek)`, true)
+    .addField(`Disbord`,`!d bump dans le chat`, true)
+    .addField(`DiscordDL`,`!$ bump dans le chat ou [ici](https://www.discordl.org/serveurs/778368411620474910)`, true)
+    .addField(`1 vote par IP toutes les 2 heures !`)
 
 client.channels.cache.get('779443908647714819').send(embed);
 
