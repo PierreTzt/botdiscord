dee7d930cc50f97231634d12a3ded65394b84954
cornergeek <76401152+cornergeek@users.noreply.github.com>
2021-01-13 11:56:36 +0100

    Update ready.js

--- events/logs/ready.js
+++ events/logs/ready.js
@@ -1,3 +1,10 @@
 module.exports = client => {
-  console.log(`Logged in as ${client.user.tag}!`)
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({
+    status: 'online',
+    activity: {
+        name: 'TEXTE',
+        type: 'LISTENING',
+    }
+});
 }
