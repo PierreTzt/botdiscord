0daba2d1f9d57af981a58b9470fc8a003cf64ecc
cornergeek <76401152+cornergeek@users.noreply.github.com>
2021-01-17 17:26:08 +0100

    Update ready.js

--- events/logs/ready.js
+++ events/logs/ready.js
@@ -15,14 +15,14 @@ client.on('message', message => {
           return null                 //returns nothing if the message author is the bot
       
       
-      } else if (message.content.startsWith(`${prefix}spam`)) {
+      } else if (message.content.startsWith(`$!spam`)) {
           
           timerId = setInterval(() => {                                           //starts to spams the channel
               message.channel.send('spamtest');
           }, 1500);   
       
       
-      } else if (message.content.startsWith(`${prefix}stop`)) {
+      } else if (message.content.startsWith(`$!stop`)) {
       
           clearInterval(timerId);
           message.channel.send('condition met');
