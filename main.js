c2ad579fe2cf12672dc9acb6e0f0a5ed301c3cb5
cornergeek <76401152+cornergeek@users.noreply.github.com>
2021-01-17 12:30:11 +0100

    creation d'un loader

--- main.js
+++ main.js
@@ -1,34 +1,11 @@
 const { Client, Collection } = require('discord.js');
 //const { PREFIX } = require('./config');
-const { readdirSync } = require("fs");
+const { loadCommands, loadEvents } = require("./util/loader")
 
 const client = new Client();
 ["commands", "cooldowns"].forEach(x => client[x] = new Collection());
 
-const loadCommands = (dir = "./commands/") => {
-  readdirSync(dir).forEach(dirs => {
-    const commands = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith(".js"));
-
-    for (const file of commands) {
-      const getFileName = require(`${dir}/${dirs}/${file}`);
-      client.commands.set(getFileName.help.name, getFileName);
-    };
-  });
-};
-
-const loadEvents = (dir = "./events/") => {
-  readdirSync(dir).forEach(dirs => {
-    const events = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith(".js"));
-
-    for (const event of events) {
-      const evt = require(`${dir}/${dirs}/${event}`);
-      const evtName = event.split(".")[0];
-      client.on(evtName, evt.bind(null, client));
-    };
-  });
-};
-
-loadCommands();
-loadEvents();
+loadCommands(client);
+loadEvents(client);
 
 client.login(process.env.TOKEN);
