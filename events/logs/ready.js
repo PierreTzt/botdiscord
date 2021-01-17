5fb178974a6cf68b56db595266f0c98cf6e11493
cornergeek <76401152+cornergeek@users.noreply.github.com>
2021-01-17 17:22:57 +0100

    Update ready.js

--- events/logs/ready.js
+++ events/logs/ready.js
@@ -1,4 +1,6 @@
-module.exports = client => {
+const { MessageEmbed } = require("discord.js");
+
+module.exports = (client) => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setPresence({
     status: 'online',
@@ -7,4 +9,25 @@ module.exports = client => {
         type: 'WATCHING',
     }
 });
+
+bot.on('message', message => {
+        if(message.author.bot) {
+          return null                 //returns nothing if the message author is the bot
+      
+      
+      } else if (message.content.startsWith(`${prefix}spam`)) {
+          
+          timerId = setInterval(() => {                                           //starts to spams the channel
+              message.channel.send('spamtest');
+          }, 1500);   
+      
+      
+      } else if (message.content.startsWith(`${prefix}stop`)) {
+      
+          clearInterval(timerId);
+          message.channel.send('condition met');
+      
+      }
+    }
+)
 }
