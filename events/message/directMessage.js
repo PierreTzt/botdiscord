0a8c0bb213540b7ceda88f4cf9f90c3c89c7ebff
cornergeek <76401152+cornergeek@users.noreply.github.com>
2021-01-17 12:19:58 +0100

    Update directMessage.js

--- events/message/directMessage.js
+++ events/message/directMessage.js
@@ -1,3 +1,15 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = (client, message) => {
-  console.log("Je suis l'évenement directMessage!");
+  const user = message.author;
+
+  const embed = new MessageEmbed()
+    .setAuthor(`${user.username} (${user.id})`)
+    .setColor("#ffa500")
+    .setDescription(`**Action** : Ouverture ticket\n**Raison** : ${message.content}`)
+    .setThumbnail(user.avatarURL())
+    .setTimestamp()
+    .setFooter(message.author.username, message.author.avatarURL());
+
+  client.channels.cache.get('779443908647714819').send(embed);
 }
